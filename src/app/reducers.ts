6303e5c7d80a1fab4ef59e26d27cd3e64b5c1ebd
Anthony Lukach <alukach@cadasta.org>
2018-06-03 23:37:26 -0600

    Change localstorage persistence lib

--- src/app/reducers.ts
+++ src/app/reducers.ts
@@ -1,31 +1,37 @@
 import { combineReducers } from "redux";
 import { compose, createStore } from "redux";
-import persistState from "redux-localstorage";
+import { PersistConfig, persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { AuthAction } from "../auth/authActions";
 import { authReducer, AuthState } from "../auth/authReducer";
 import { CustomWindow } from "../shared/custom.window";
 declare let window: CustomWindow;
 
+// Reducers
+export const rootReducer = combineReducers({
+  auth: authReducer
+});
+const persistConfig: PersistConfig = { key: "root", storage };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Initial State
 export interface StoreState {
   auth: AuthState | null;
 }
 const initialState: StoreState = {
   auth: null
 };
-type Actions = AuthAction;
 
-export const rootReducer = combineReducers({
-  auth: authReducer
-});
-
+// Enhancers
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhancer = composeEnhancers(
-  persistState() // TODO: Only persist auth slice
-);
+const enhancer = composeEnhancers();
 
+// Store
+type Actions = AuthAction;
 export const store = createStore<StoreState, Actions, any, any>(
-  rootReducer,
+  persistedReducer,
   initialState,
   enhancer
 );
+export const persistor = persistStore(store);
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,20 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./app/App";
-import { store } from "./app/reducers";
+import { persistor, store } from "./app/reducers";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <PersistGate persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
   </Provider>,
   document.getElementById("root") as HTMLElement
 );
