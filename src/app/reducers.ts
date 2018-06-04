34e7961fc9a2f4d70b6090f9185538ea36a7ec77
Anthony Lukach <alukach@cadasta.org>
2018-06-03 18:24:18 -0600

    Sync store with localStorage

--- src/app/reducers.ts
+++ src/app/reducers.ts
@@ -1,23 +1,31 @@
-import { combineReducers } from 'redux';
-import { createStore } from 'redux';
+import { combineReducers } from "redux";
+import { compose, createStore } from "redux";
+import persistState from "redux-localstorage";
 
-import { AuthAction } from '../auth/authActions';
-import { authReducer, AuthState } from '../auth/authReducer';
-import { CustomWindow } from '../shared/custom.window';
+import { AuthAction } from "../auth/authActions";
+import { authReducer, AuthState } from "../auth/authReducer";
+import { CustomWindow } from "../shared/custom.window";
 declare let window: CustomWindow;
 
 export interface StoreState {
   auth: AuthState | null;
 }
 const initialState: StoreState = {
-  auth: null,
+  auth: null
 };
 type Actions = AuthAction;
 
 export const rootReducer = combineReducers({
-  auth: authReducer,
+  auth: authReducer
 });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(
+  persistState() // TODO: Only persist auth slice
+);
+
 export const store = createStore<StoreState, Actions, any, any>(
-  rootReducer, initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  rootReducer,
+  initialState,
+  enhancer
 );
--- src/shared/custom.window.ts
+++ src/shared/custom.window.ts
@@ -1,3 +1,4 @@
 export interface CustomWindow extends Window {
   __REDUX_DEVTOOLS_EXTENSION__?: () => any;
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: any;
 }
