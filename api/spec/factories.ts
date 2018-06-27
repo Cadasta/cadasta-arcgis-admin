e219118526916dadbd10a8719e9d6ce8b6b145d3
Oliver Roick <oliver.roick@gmail.com>
2018-06-27 16:10:43 +0200

    Add projects/create tests

--- api/spec/factories.ts
+++ api/spec/factories.ts
@@ -15,3 +15,43 @@ export const userResponseFactory = (userDetails = {}) => responseBodyFactory(
     ...userDetails
   }
 );
+
+export const APIGatewayProxyEventFactory = (body: {}, method: string = 'GET'): AWSLambda.APIGatewayProxyEvent => {
+  return {
+    body: JSON.stringify(body),
+    headers: {},
+    httpMethod: method,
+    isBase64Encoded: false,
+    path: '/projects/',
+    pathParameters: null,
+    queryStringParameters: null,
+    stageVariables: null,
+    requestContext: {
+      accountId: 'oroick',
+      apiId: 'API',
+      httpMethod: method,
+      identity: {
+        accessKey: null,
+        accountId: null,
+        apiKey: null,
+        apiKeyId: null,
+        caller: null,
+        cognitoAuthenticationProvider: null,
+        cognitoAuthenticationType: null,
+        cognitoIdentityId: null,
+        cognitoIdentityPoolId: null,
+        sourceIp: '127.0.0.1',
+        user: null,
+        userAgent: null,
+        userArn: null,
+      },
+    path: '/projects/',
+    stage: 'test',
+    requestId: '1',
+    requestTimeEpoch: 0,
+    resourceId: '1',
+    resourcePath: '1',
+    },
+    resource: 'string'
+  };
+};
