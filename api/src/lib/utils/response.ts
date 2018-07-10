36050a759961ea3b47c1b8f5147d91c36043fb91
Anthony Lukach <alukach@cadasta.org>
2018-07-10 12:07:26 -0600

    Better structure around response helper functions

--- api/src/lib/utils/response.ts
+++ api/src/lib/utils/response.ts
@@ -1,9 +1,21 @@
-export const buildResponse = (body: {[key: string]: any}, statusCode = 200, headers = {}): AWSLambda.APIGatewayProxyResult => ({
+interface ResponseBody {
+  [key: string]: any;
+}
+interface ErrResponseBody {
+  msg: string;
+  err: string;
+}
+interface ResponseFunction <TResponse> {
+  (body: TResponse, statusCode?: number, headers?: {[key: string]: string}): AWSLambda.APIGatewayProxyResult
+}
+
+export const response: ResponseFunction<ResponseBody> = (body, statusCode = 200, headers = {}) => ({
   statusCode,
   body: JSON.stringify(body),
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
     ...headers
-  },
-})
+  }
+});
+export const errResponse = response as ResponseFunction<ErrResponseBody>;
